1e4ca396735d3e7c510ccc680a987413a85e78dc
Habidhossen <habidhossen2@gmail.com>
2022-05-03 19:43:57 +0600

    mongoDB drive code added

--- index.js
+++ index.js
@@ -8,10 +8,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// mongoDB drive code
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3mjkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+// running the server
 app.get("/", (req, res) => {
   res.send("Running the server...");
 });
 
+// listening port
 app.listen(port, () => {
   console.log("Listening to port", port);
 });
