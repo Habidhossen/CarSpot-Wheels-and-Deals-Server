08a8f16438c60e41bd365ae51f5e0fb489e3f43f
Habidhossen <habidhossen2@gmail.com>
2022-05-04 14:44:31 +0600

    update api added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // mongoDB drive code
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3mjkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -25,7 +25,7 @@ async function run() {
 
     // POST Product
     // http://localhost:5000/product
-    app.post("/product", async (req, res) => {
+    app.post("/inventory", async (req, res) => {
       const data = req.body;
       const result = await productsCollection.insertOne(data);
       res.send(result);
@@ -33,11 +33,38 @@ async function run() {
 
     // GET Product
     // http://localhost:5000/products
-    app.get("/products", async (req, res) => {
+    app.get("/inventory", async (req, res) => {
       // const query = req.query;
       const query = {};
       const cursor = productsCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // GET One Product by ID
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    // UPDATE Product by ID
+    app.put("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          //
+        },
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
     });
 
