a5a97f676ea0d7a9ffcdb4678405e10d5f87e229
Habidhossen <habidhossen2@gmail.com>
2022-05-04 15:15:06 +0600

    correction

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 // mongoDB drive code
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3mjkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -43,7 +44,7 @@ async function run() {
     app.get("/inventory/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const result = await userCollection.findOne(query);
+      const result = await productsCollection.findOne(query);
       res.send(result);
     });
 
